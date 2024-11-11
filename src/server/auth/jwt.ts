bd223c66d8e1cb2c8410662adc66821041305918
Kazuma Satō <145659895+sato-kazuma-kun@users.noreply.github.com>
2024-11-11 12:32:04 +0530

    Bug fixes

--- src/server/auth/jwt.ts
+++ src/server/auth/jwt.ts
@@ -2,7 +2,7 @@ import { SignJWT, jwtVerify } from "jose";
 import { expires } from "@/server/utils";
 
 const secretKey = process.env.AUTH_SECRET;
-if (!secretKey) throw new Error("Secret key environment variable is undefined.");
+if (!secretKey) throw new Error("AUTH_SECRET environment variable is undefined.");
 const key = new TextEncoder().encode(secretKey);
 
 export async function encrypt(payload: any) {
